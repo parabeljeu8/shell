1f7ce3374a4040ef29fb94d897809a717449b533
Patricia R <rabelpatricia@gmail.com>
2025-11-13 10:18:30 -0500

    one more push to reflect changes

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -47,7 +47,6 @@ rm ./data/processed/user_logs/*ipaddr*
 ls ./data/processed/*/* > ./data/inventory.txt
 
 ###########################################
-#######################################
 
 
 echo "Project setup is complete!"
